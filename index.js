b385df5b20925f516454363e694273434325af9e
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-02-17 15:58:40 +0600

    basic node mongo setup

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qtxjg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res) => {
     res.send('Invent server is running')
---
+++ task.js
@@ -0,0 +1,9 @@
+/* 
+
+Things I have done in 17,02,2023
+
+1. Create new DataBase for INvent
+2. Connected it using (username and password)
+3. create a .env file to keep the secret data saved and place it in .gitignore to keep it safe.
+
+*/
